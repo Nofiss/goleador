2f207320a51586d5f4fede9470ef5324580494e1
sbernagozzi-csw <bernasimone97@gmail.com>
2026-01-24 00:23:08 +0100

    chore: user soft delete migration

--- src/backend/Goleador.Infrastructure/Migrations/20260123231822_UserSoftDelete.Designer.cs
+++ src/backend/Goleador.Infrastructure/Migrations/20260123231822_UserSoftDelete.Designer.cs
@@ -4,6 +4,7 @@ using Goleador.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Goleador.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260123231822_UserSoftDelete")]
+    partial class UserSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
