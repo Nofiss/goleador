d5af896b590014e18cdc9ecd4aef9941eb7c6771
sbernagozzi-csw <bernasimone97@gmail.com>
2026-01-24 22:18:59 +0100

    chore: migrate elo ratings

---
+++ src/backend/Goleador.Infrastructure/Migrations/20260124211830_EloRatings.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Goleador.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EloRatings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EloRating",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 1200,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EloRating",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 1200);
+        }
+    }
+}
